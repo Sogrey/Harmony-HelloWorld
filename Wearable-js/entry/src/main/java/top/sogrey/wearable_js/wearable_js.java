705a153f671988f14a4f41dc1104b3f8d5a56c76
Sogrey <suzhaoheng2006@163.com>
2020-09-19 09:54:07 +0800

    add Wearable-js

---
+++ Wearable-js/entry/src/main/java/top/sogrey/wearable_js/wearable_js.java
@@ -0,0 +1,10 @@
+package top.sogrey.wearable_js;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class wearable_js extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ Wearable-js/entry/src/test/java/top/sogrey/wearable_js/MainAbilityTest.java
@@ -0,0 +1,9 @@
+package top.sogrey.wearable_js;
+
+import org.junit.Test;
+
+public class MainAbilityTest {
+    @Test
+    public void onStart() {
+    }
+}
