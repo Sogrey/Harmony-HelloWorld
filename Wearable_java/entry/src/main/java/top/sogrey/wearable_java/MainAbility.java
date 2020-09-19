a8455ddec28ebf8b74788fa78e81a31e30b099d0
Sogrey <suzhaoheng2006@163.com>
2020-09-19 10:11:20 +0800

    add Wearable_java

---
+++ Wearable_java/entry/src/main/java/top/sogrey/wearable_java/MainAbility.java
@@ -0,0 +1,13 @@
+package top.sogrey.wearable_java;
+
+import top.sogrey.wearable_java.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+}
---
+++ Wearable_java/entry/src/main/java/top/sogrey/wearable_java/wearable_java.java
@@ -0,0 +1,10 @@
+package top.sogrey.wearable_java;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class wearable_java extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
